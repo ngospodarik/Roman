ee0d48763e01e93fcff74e64f4e066114f97caf7
Nikolay Gospodarik <n@gospodarik.ru>
2024-02-10 15:13:21 +0300

    complete function delivered

--- Roman.java
+++ Roman.java
@@ -22,11 +22,8 @@ public class Roman {
 
         roman = "M".repeat(thousands);
 
-        if (remainder_from_thousands / 100 == 9 && five_hundreds == 0 ) {
-            roman += "CM";
-        }
-
-        if (five_hundreds != 0) roman += "D";
+        if (remainder_from_thousands / 100 == 9) roman += "CM";
+        else if (remainder_from_thousands / 100 == 9 && five_hundreds != 0) roman += "D";
 
         if (remainder_from_thousands / 100 != 9) {
 
@@ -35,17 +32,41 @@ public class Roman {
             } else roman += "C".repeat(hundreds);
         }
 
-        if (fifties == 4) roman += "XC";
+        if (remainder_from_hundreds == 90) {
 
-        if (fifties != 4 && fifties != 0) roman += "L";
+            roman += "XC";
 
-        if (tens == 4) roman += "XL";
+        } else {
 
-        if (tens != 4 && tens != 0) roman += "X".repeat(tens);
+            if (fifties == 4) roman += "XC";
 
-        // if self.remainder_from_tens:
-        // self.roman += Roman.digits_dict[self.remainder_from_tens]
+            if (fifties != 4 && fifties != 0) roman += "L";
 
+            if (tens == 4) roman += "XL";
+
+            if (tens != 4 && tens != 0) roman += "X".repeat(tens);
+        }
+
+        switch (remainder_from_tens) {
+            case 1: roman += "I";
+            break;
+            case 2: roman += "II";
+            break;
+            case 3: roman += "III";
+            break;
+            case 4: roman += "IV";
+            break;
+            case 5: roman += "V";
+            break;
+            case 6: roman += "VI";
+            break;
+            case 7: roman += "VII";
+            break;
+            case 8: roman += "VIII";
+            break;
+            case 9: roman += "IX";
+            break;
+        }
         System.out.println(roman);
     }
 
